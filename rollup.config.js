f3e9d0085b90c792d86e30b8b82d550f669ac5d5
Krit B <krit@screencloud.io>
2022-04-18 00:14:30 +0700

    fix(package): fix to export type correctly

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
+import dts from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
@@ -30,5 +31,11 @@ export default [
             terser(),
             json()
         ],
+    },
+    {
+        input: "dist/esm/types/index.d.ts",
+        output: [{ file: "dist/index.d.ts", format: "esm" }],
+        plugins: [dts()],
+        external: [/\.css$/],
     },
 ];
